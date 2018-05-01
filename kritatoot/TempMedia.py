4eaaa29ce5bba02f08615c486204b242fa18b566
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2018-05-01 19:42:57 +0200

    Make KritaToot check the filename for png|jpeg|gif and if not, try to export as png.
    This doesn't always seem to work 100%? There seems to be a timing issue, but my debug messages aren't working
    properly on this device, so I'll need to fix that first before I can fix this.

--- kritatoot/TempMedia.py
+++ kritatoot/TempMedia.py
@@ -2,6 +2,7 @@
 import os
 import inspect
 from tempfile import gettempdir
+import re
 
 
 from krita import *
@@ -30,10 +31,21 @@ def saveTempMedia():
         if not docname:
             docname = 'noname.png'
         
+        # Check if the file in question is one that can be uploaded to mastodon
+
+        match = re.search(r"\w+\.png|jpeg|gif|jpg", docname)
+
+        # If not, save as a png.
+        if match is None:
+            docname = re.sub(r"(\.\w+)", ".png", docname, flags=re.IGNORECASE)
+
         tempbase = os.path.basename(docname)
         pathname = os.path.join(tempdir, tempbase)
         
+        batchmode = doc.batchmode()
+        doc.setBatchmode(True)
         doc.exportImage(pathname, InfoObject())
+        doc.setBatchmode(batchmode)
         
         print('export current doc as %s' % pathname)
         
